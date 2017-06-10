641cba1904f01a91db87bbe19c571afa50ddf2e1
Mayank Jain <jainmickey93@gmail.com>
2017-06-11 00:38:33 +0530

    fix(flickr): fix json type conversion bug

--- jainmickey/flickr/services.py
+++ jainmickey/flickr/services.py
@@ -15,7 +15,7 @@ def get_photo_stream_from_flickr():
     Return a photo stream json fetched from flickr api.
     """
     return json.loads(flickr.photosets.getPhotos(photoset_id=settings.FLICKR_PHOTO_SET_ID,
-                                                 extras='url_o,url_m,owner_name,views,date_upload'))
+                                                 extras='url_o,url_m,owner_name,views,date_upload').decode("utf-8"))
 
 
 def get_photo_from_flickr(photo_id):
@@ -23,4 +23,4 @@ def get_photo_from_flickr(photo_id):
     Return a photo detail json fetched from flickr api.
     :photo_id
     """
-    return json.loads(flickr.photos.getInfo(photo_id=photo_id))
+    return json.loads(flickr.photos.getInfo(photo_id=photo_id).decode("utf-8"))
